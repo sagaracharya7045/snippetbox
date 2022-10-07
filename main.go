4e8453b293c23cd36d2a2e142a464eaca7e42193
Sagar Acharya <sagar.acharya7045@gmail.com>
2022-10-07 13:38:15 +0545

    Basic WebServer Setup

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from SnippetBox"))
+}
 func main() {
-	fmt.Println("Hello")
+	// fmt.Println("Hello")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting Server on : 4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
