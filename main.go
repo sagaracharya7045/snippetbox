5c8d503f60d6a338caf93bae3ce2c5aa5c17775d
Sagar Acharya <sagar.acharya7045@gmail.com>
2022-10-11 20:54:50 +0545

    change from GitDesktop
    change from git desktop

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
-	// Check if the current request URL path exactly matches "/". If it doesn't, use
-	// the http.NotFound() function to send a 404 response to the client.
+	// Check if the current request URL path exactly matches "/". If it doesn't, useAAAA
 	// Importantly, we then return from the handler. If we don't return the handler
 	// would keep executing and also write the "Hello from SnippetBox" message.
 	log.Println(r.URL)
